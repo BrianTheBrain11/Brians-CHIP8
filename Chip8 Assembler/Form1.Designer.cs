2d3c8cfe1a8f5f7b394584c3a9a9040ddc36ecb0
BrianTheBrain11 <68790720+BrianTheBrain11@users.noreply.github.com>
2025-03-26 23:59:58 -0700

    add saving loading of .s files, DRW, LD, CLS

--- Chip8 Assembler/Form1.Designer.cs
+++ Chip8 Assembler/Form1.Designer.cs
@@ -32,6 +32,13 @@
             AssembleButton = new Button();
             OutputFileDialog = new SaveFileDialog();
             OutputFileBrowseButton = new Button();
+            menuStrip = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            saveToFileToolStripMenuItem = new ToolStripMenuItem();
+            loadFromFileToolStripMenuItem = new ToolStripMenuItem();
+            saveFileDialog = new SaveFileDialog();
+            openFileDialog = new OpenFileDialog();
+            menuStrip.SuspendLayout();
             SuspendLayout();
             // 
             // ProgramTextBox
@@ -39,10 +46,10 @@
             ProgramTextBox.AccessibleDescription = "Space for inputting Chip8 program.";
             ProgramTextBox.AccessibleName = "Program Text Box";
             ProgramTextBox.AccessibleRole = AccessibleRole.Document;
-            ProgramTextBox.Location = new Point(22, 65);
+            ProgramTextBox.Location = new Point(22, 82);
             ProgramTextBox.Multiline = true;
             ProgramTextBox.Name = "ProgramTextBox";
-            ProgramTextBox.Size = new Size(979, 571);
+            ProgramTextBox.Size = new Size(979, 554);
             ProgramTextBox.TabIndex = 0;
             ProgramTextBox.TextChanged += ProgramTextBox_TextChanged;
             // 
@@ -51,7 +58,7 @@
             AssembleButton.AccessibleDescription = "Button to being assembly";
             AssembleButton.AccessibleName = "AssembleButton";
             AssembleButton.AccessibleRole = AccessibleRole.PushButton;
-            AssembleButton.Location = new Point(22, 16);
+            AssembleButton.Location = new Point(22, 27);
             AssembleButton.Name = "AssembleButton";
             AssembleButton.Size = new Size(94, 37);
             AssembleButton.TabIndex = 1;
@@ -67,14 +74,59 @@
             // 
             OutputFileBrowseButton.AccessibleDescription = "Button to select the file to which the program will be saved to.";
             OutputFileBrowseButton.AccessibleName = "Output file browse";
-            OutputFileBrowseButton.Location = new Point(127, 16);
+            OutputFileBrowseButton.Location = new Point(122, 27);
             OutputFileBrowseButton.Name = "OutputFileBrowseButton";
             OutputFileBrowseButton.RightToLeft = RightToLeft.Yes;
-            OutputFileBrowseButton.Size = new Size(874, 37);
+            OutputFileBrowseButton.Size = new Size(879, 37);
             OutputFileBrowseButton.TabIndex = 2;
             OutputFileBrowseButton.TextAlign = ContentAlignment.MiddleRight;
             OutputFileBrowseButton.UseVisualStyleBackColor = true;
             OutputFileBrowseButton.Click += OutputFileBrowseButton_Click;
+            // 
+            // menuStrip
+            // 
+            menuStrip.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip.Location = new Point(0, 0);
+            menuStrip.Name = "menuStrip";
+            menuStrip.Size = new Size(1027, 24);
+            menuStrip.TabIndex = 3;
+            menuStrip.Text = "menuStrip1";
+            menuStrip.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { saveToFileToolStripMenuItem, loadFromFileToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // saveToFileToolStripMenuItem
+            // 
+            saveToFileToolStripMenuItem.AccessibleDescription = "Save whats currently in the program text box to a file.";
+            saveToFileToolStripMenuItem.AccessibleName = "Save to file";
+            saveToFileToolStripMenuItem.AccessibleRole = AccessibleRole.MenuItem;
+            saveToFileToolStripMenuItem.Name = "saveToFileToolStripMenuItem";
+            saveToFileToolStripMenuItem.Size = new Size(148, 22);
+            saveToFileToolStripMenuItem.Text = "Save to file";
+            saveToFileToolStripMenuItem.Click += SaveToFileToolStripMenuItem_Click;
+            // 
+            // loadFromFileToolStripMenuItem
+            // 
+            loadFromFileToolStripMenuItem.AccessibleDescription = "Loads an assembly file and puts the contents in the program text box.";
+            loadFromFileToolStripMenuItem.AccessibleName = "Load from file";
+            loadFromFileToolStripMenuItem.Name = "loadFromFileToolStripMenuItem";
+            loadFromFileToolStripMenuItem.Size = new Size(148, 22);
+            loadFromFileToolStripMenuItem.Text = "Load from file";
+            loadFromFileToolStripMenuItem.Click += LoadFromFileToolStripMenuItem_Click;
+            // 
+            // saveFileDialog
+            // 
+            saveFileDialog.Filter = "Chip8 Assembly file (*.s)|*.s|All Files (*.*)|*.*";
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.Filter = "Chip8 Assembly file (*.s)|*.s|All Files (*.*)|*.*";
+            openFileDialog.FileOk += openFileDialog_FileOk;
             // 
             // Form1
             // 
@@ -84,8 +136,12 @@
             Controls.Add(OutputFileBrowseButton);
             Controls.Add(AssembleButton);
             Controls.Add(ProgramTextBox);
+            Controls.Add(menuStrip);
+            MainMenuStrip = menuStrip;
             Name = "Form1";
             Text = "Form1";
+            menuStrip.ResumeLayout(false);
+            menuStrip.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,5 +152,11 @@
         private Button AssembleButton;
         private SaveFileDialog OutputFileDialog;
         private Button OutputFileBrowseButton;
+        private MenuStrip menuStrip;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem saveToFileToolStripMenuItem;
+        private ToolStripMenuItem loadFromFileToolStripMenuItem;
+        private SaveFileDialog saveFileDialog;
+        private OpenFileDialog openFileDialog;
     }
 }
--- Chip8 Assembler/Form1.cs
+++ Chip8 Assembler/Form1.cs
@@ -1,10 +1,113 @@
+using System.Runtime.InteropServices;
+
 namespace Chip8_Assembler
 {
     public partial class Form1 : Form
     {
+        Dictionary<string, Func<object[], UInt16>> operations = new Dictionary<string, Func<object[], UInt16>>();
+
         public Form1()
         {
             InitializeComponent();
+
+            ConsoleAllocator.ShowConsoleWindow();
+
+
+            operations["LD"] = (args) =>
+            {
+                if (args.Length != 3)
+                {
+                    throw new ArgumentException("Wrong number of arguments for LD");
+                }
+
+                char firstCharOfArg1 = ((string)args[1])[0];
+                if (firstCharOfArg1 != 'V' && firstCharOfArg1 != 'I')
+                {
+                    throw new ArgumentException("First argument is not a V register or I");
+                }
+
+                if (firstCharOfArg1 == 'I')
+                {
+                    byte opcode = (byte)0xA;
+                    UInt16 value = (UInt16)int.Parse((string)args[2]);
+                    byte sourceRegister = (byte)int.Parse(((string)args[2]).AsSpan(1));
+                    UInt16 op = (UInt16)
+                    (
+                        (value) | // bits 0-11
+                        (opcode << 12) // bits 12-15
+                    );
+                    return op;
+                }
+
+                byte targetRegister = (byte)int.Parse(((string)args[1]).AsSpan(1));
+
+                char firstCharOfArg2 = ((string)args[2])[0];
+                if (firstCharOfArg2 == 'V')
+                {
+                    byte opcode = (byte)0x8;
+                    byte sourceRegister = (byte)int.Parse(((string)args[2]).AsSpan(1));
+                    UInt16 op = (UInt16)
+                    (
+                        ((byte)0x00) | // bits 0-3
+                        (sourceRegister << 4) | // bits 3-7
+                        (targetRegister << 8) | // bits 8-11
+                        (opcode << 12) // bits 12-15
+                    );
+                    return op;
+                }
+                else
+                {
+
+                    byte opcode = (byte)0x6;
+                    byte value = (byte)int.Parse((string)args[2]);
+                    UInt16 op = (UInt16)
+                    (
+                        (value) | // bits 0-7
+                        (targetRegister << 8) | // bits 8-11
+                        (opcode << 12) // bits 12-15
+                    );
+                    return op;
+                }
+            };
+            operations["CLS"] = (args) =>
+            {
+                return (UInt16)0x00E0;
+            };
+            operations["DRW"] = (args) =>
+            {
+                Console.WriteLine("In a draw op");
+                if (args.Length != 4)
+                {
+                    throw new ArgumentException("Wrong number of arguments for DRW");
+                }
+                char firstCharOfArg1 = ((string)args[1])[0];
+                if (firstCharOfArg1 != 'V')
+                {
+                    throw new ArgumentException("First argument is not a V register");
+                }
+                char firstCharOfArg2 = ((string)args[2])[0];
+                if (firstCharOfArg2 != 'V')
+                {
+                    throw new ArgumentException("First argument is not a V register");
+                }
+
+                byte opcode = 0xD;
+
+                byte argumentRegister1 = (byte)int.Parse(((string)args[1]).AsSpan(1));
+                byte argumentRegister2 = (byte)int.Parse(((string)args[2]).AsSpan(1));
+
+                byte drawBytesCount = (byte)int.Parse((string)args[3]);
+
+                UInt16 op = (UInt16)
+                (
+                    (drawBytesCount) | // bits 0-3
+                    (argumentRegister2 << 4) | // bits 3-7
+                    (argumentRegister1 << 8) | // bits 8-11
+                    (opcode << 12) // bits 12-15
+                );
+
+                return op;
+            };
         }
 
         private void ProgramTextBox_TextChanged(object sender, EventArgs e)
@@ -14,10 +117,24 @@ namespace Chip8_Assembler
 
         private void AssembleButton_Click(object sender, EventArgs e)
         {
+            string path = OutputFileDialog.FileName;
+
+            List<byte> bytesToWrite = new List<byte>();
+
             foreach (string line in ProgramTextBox.Lines)
             {
+                string[] words = line.Split(' ');
+                UInt16 op = (UInt16)operations[words[0]](words);
 
+                byte highByte = (byte)(op >> 8); // upper byte
+                byte lowByte = (byte)(op & 0x00FF); // lower byte
+
+                // write high byte first because we are big endian
+                bytesToWrite.Add(highByte);
+                bytesToWrite.Add(lowByte);
             }
+
+            File.WriteAllBytes(path, bytesToWrite.ToArray());
         }
 
         private void OutputFileBrowseButton_Click(object sender, EventArgs e)
@@ -26,6 +143,69 @@ namespace Chip8_Assembler
             {
                 OutputFileBrowseButton.Text = OutputFileDialog.FileName;
             }
+        }
+
+        private void SaveToFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllLines(saveFileDialog.FileName, ProgramTextBox.Lines);
+            }
+        }
+
+        private void LoadFromFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                ProgramTextBox.Lines = File.ReadAllLines(openFileDialog.FileName);
+            }
+        }
+
+        private void openFileDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+
+    internal static class ConsoleAllocator
+    {
+        [DllImport(@"kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [DllImport(@"kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport(@"user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        const int SwHide = 0;
+        const int SwShow = 5;
+
+
+        public static void ShowConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            if (handle == IntPtr.Zero)
+            {
+                AllocConsole();
+            }
+            else
+            {
+                ShowWindow(handle, SwShow);
+            }
+        }
+
+        public static void HideConsoleWindow()
+        {
+            var handle = GetConsoleWindow();
+
+            ShowWindow(handle, SwHide);
         }
     }
 }
